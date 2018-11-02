c2705300bb2597d89ef5aa5cad735120699755ac
Chris DuPuis <cpdupuis@gmail.com>
2018-11-01 21:11:08 -0700

    Initial commit

---
+++ learningjava/src/main/java/com/bitsforabetterworld/learningjava/App.java
@@ -0,0 +1,14 @@
+package com.bitsforabetterworld.learningjava;
+
+/*
+
+
+*/
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
